a8dbe48069ee3f01bd202cf146b5b4e46799b8f2
mkpoojitha-dotcom <mkpoojitha216@gmail.com>
2025-10-27 10:44:28 -0700

    Update grade_analysis.php

--- lab_functions_arrays/grade_analysis.php
+++ lab_functions_arrays/grade_analysis.php
@@ -1,16 +1,16 @@
-$students ={
+$students =[
     'Daniel' => 85,
     'Matthew' => 92,
     'Simon' => 78,
-    'Lily' = 88,
+    'Lily' => 88,
     'Shondel' => 95
-};
+];
 $scores = array_values($students);
 
 $classAverage = average($scores);
 echo "Class Average is " . round($classAverage, 2) . "\n";
 
-foreach ($student as $name => $score){
+foreach ($students as $name => $score){
     $grade = letterGrade($score);
-    echo "$name: $score -> $garde\n";
+    echo "$name: $score -> $grade\n";
 }
