81f2dfb6eb3347e145565d41ea4f69baea804fde
mkpoojitha-dotcom <mkpoojitha216@gmail.com>
2025-10-27 08:51:05 -0700

    Update letter_grade.php

--- lab_functions_arrays/letter_grade.php
+++ lab_functions_arrays/letter_grade.php
@@ -1,1 +1,12 @@
 
+function letterGrade($score){
+    if($score >= 90){
+        return 'A';
+    }else if ($score >= 80){
+        return 'B';
+    }else if($score >= 70){
+        return 'D';
+    }else{
+        return 'F';
+    }
+}
